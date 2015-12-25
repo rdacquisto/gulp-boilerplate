b501b0e9b0852e935ef2e7527b018843faf58e80
bellmounte <ryan.dacquisto@gmail.com>
2015-12-24 20:12:38 -0800

    Updating HTML minification plugin.

--- gulp/html.js
+++ gulp/html.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var minifyHTML = require('gulp-minify-html');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('html', function () {
 	gutil.log(gutil.colors.bgBlue('HTML Task'));
 
 	gulp.src(global.sourcePath + '**/*.htm*')
-		.pipe(minifyHTML({empty:true,conditionals:true}))
+		.pipe(htmlmin({collapseWhitespace: true}))
 		.pipe(gulp.dest(global.buildPath));
 });
